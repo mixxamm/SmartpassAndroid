8ada7f3eccec9ed60add513b8d7a1e99a1077de5
mixxamm <33133459+mixxamm@users.noreply.github.com>
2017-12-08 18:49:28 +0100

    Voeg app-icoontje in launcher en app-logo in app toe.

--- app/src/main/java/com/mixxamm/smartpassalpha/MainActivity.java
+++ app/src/main/java/com/mixxamm/smartpassalpha/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mixxamm.smartpassalpha;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 
 import static com.mixxamm.smartpassalpha.R.id.smartschool_login;
@@ -16,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*VideoView videoView = (VideoView) findViewById(R.id.videoView1);
+        Uri path = Uri.parse("android.recourse://" + getPackageName() + "/" + R.raw.video);
+        videoView.setVideoURI(path);
+        videoView.start();*/
         ImageView smartschoolLogin = (ImageView) findViewById(smartschool_login);
         smartschoolLogin.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
