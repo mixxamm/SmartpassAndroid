0324fe0e14c57ca046164fdcd588be0b4b6ea5f9
mixxamm <33133459+mixxamm@users.noreply.github.com>
2017-12-28 14:35:14 +0100

    Toon tijd in leerlingen- en leerkrachten-activiteit
    Dit zorgt ervoor dat het geen zin heeft als leerlingen screenshots nemen, aangezien de tijd tot op de seconde wordt getoond. Bij twijfel kan de leerkracht uiteraard de QR-code scannen. Ook op de leerkrachten-activiteit wordt de tijd getoond, zodat deze kan worden vergeleken met wat op de smartphone van de leerlingen staat. En het is natuurlijk ook altijd handig om te weten hoe laat het is.

--- app/src/main/java/com/mixxamm/smartpassalpha/LoginTest.java
+++ app/src/main/java/com/mixxamm/smartpassalpha/LoginTest.java
@@ -1,7 +1,9 @@
 package com.mixxamm.smartpassalpha;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import static com.mixxamm.smartpassalpha.R.*;
 
 
 public class LoginTest extends AppCompatActivity {
@@ -17,22 +20,28 @@ public class LoginTest extends AppCompatActivity {
     ProgressBar progressBar;
     EditText Gebruikersnaam, Wachtwoord;
 
+    //Voorkeuren
+    private static final String PREFS= "preferences";
+    private static final String PREF_GEBRUIKERSNAAM = "gebruikersnaam";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_test);
-        Gebruikersnaam = (EditText)findViewById(R.id.gebruikersnaamtest);
-        Wachtwoord = (EditText)findViewById(R.id.wachtwoordtest);
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        TextView wachtwoordInstellen = (TextView) findViewById(R.id.wachtwoordInstellen);
+        setContentView(layout.activity_login_test);
+        Gebruikersnaam = (EditText)findViewById(id.gebruikersnaamtest);
+        Wachtwoord = (EditText)findViewById(id.wachtwoordtest);
+        Button loginButton = (Button) findViewById(id.loginButton);
+        TextView wachtwoordInstellen = (TextView) findViewById(id.wachtwoordInstellen);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProgressBar progressBar = (ProgressBar) findViewById(R.id.login_laden);
+                ProgressBar progressBar = (ProgressBar) findViewById(id.login_laden);
                 progressBar.setVisibility(View.VISIBLE);
                 String gebruikersnaam = Gebruikersnaam.getText().toString();
                 String wachtwoord = Wachtwoord.getText().toString();
+                SharedPreferences sharedPreferences = LoginTest.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(PREF_GEBRUIKERSNAAM, gebruikersnaam);
                 String type = "login";//Zorgt ervoor dat de klasse login weet dat we willen inloggen. (In de toekomst kunnen we nog andere functies toevoegen)
 
                 Login login = new Login(LoginTest.this);
